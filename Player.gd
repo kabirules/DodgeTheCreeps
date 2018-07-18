b122d2c3e5e19f07b5c50022fb04e8b503da8236
kabirules <javifont@gmail.com>
2018-07-18 23:42:09 +0200

    Player movement OK

--- Player.gd
+++ Player.gd
@@ -17,20 +17,22 @@ func _ready():
 #	# Update game logic here.
 #	pass
 func _process(delta):
-    var velocity = Vector2() # The player's movement vector.
+	var velocity = Vector2() # The player's movement vector.
+	
+	if Input.is_action_pressed("ui_right"):
+	    velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+	    velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+	    velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+	    velocity.y -= 1
+	if velocity.length() > 0:
+	    velocity = velocity.normalized() * speed
+	    $AnimatedSprite.play()
+	else:
+	    $AnimatedSprite.stop()
 
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
-        $AnimatedSprite.play()
-    else:
-        $AnimatedSprite.stop()
-		
-
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
