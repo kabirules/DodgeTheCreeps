106d80243fdf97c5ea90dc76f0c7d3dbd7acac3d
kabirules <javifont@gmail.com>
2018-07-19 22:15:42 +0200

    Player collision

--- Player.gd
+++ Player.gd
@@ -3,12 +3,14 @@ extends Area2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+signal hit
 export (int) var speed  # How fast the player will move (pixels/sec).
 var screensize  # Size of the game window.
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	hide()
 	screensize = get_viewport_rect().size
 	pass
 
@@ -36,3 +38,16 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+func _on_Player_body_entered(body):
+	hide() # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.disabled = true
