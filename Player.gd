f07f25a21bc5707db6bdbc3ca75315b8353ec92f
kabirules <javifont@gmail.com>
2018-07-19 22:50:18 +0200

    Mob scene

--- Player.gd
+++ Player.gd
@@ -51,3 +51,8 @@ func _on_Player_body_entered(body):
 	hide() # Player disappears after being hit.
 	emit_signal("hit")
 	$CollisionShape2D.disabled = true
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false	
